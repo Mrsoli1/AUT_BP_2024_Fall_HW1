e919bfc6b12f8a5f4a5bcaade0b9bd1cac2db43a
Mrsoli1 <103378154+Mrsoli1@users.noreply.github.com>
2024-11-01 23:10:08 +0330

    Update Q1_NumberSign.c

--- Q1_NumberSign.c
+++ Q1_NumberSign.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
+int main() {
+    float num;
+    printf("enter your number:");
+    scanf("%.4f", &num);
+    if(num == 0)
+    {
+        printf("The number is zero.");
+    }
+    else if (num > 0)
+    {
+        printf("The number is positive.");
+    }
+    else
+    {
+        printf("The number is negative.");
+    }
 
-int main() { return 0; }
+    return 0;
+}
